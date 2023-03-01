1e2e27d217a27154bd96a71ef99a8e9ddad64605
jpunl <s-jpu2@unl.edu>
2023-03-02 09:56:25 +1100

    complete 03_12

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,14 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+function table(texture, size, numLeg, usable) {
+    this.texture = texture;
+    this.size = size;
+    this.numLeg = numLeg;
+    this.usable = usable
+};
+
+const table1 = new table("wood", 20, 4, true);
+
+console.log(table1.texture);
