f5957e9ea4e8c13d764734056452269e1e688d23
jpunl <s-jpu2@unl.edu>
2023-03-02 00:02:03 +1100

    complete prac 03_07

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,28 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const mem = {
+    volume: "16GB",
+    speed: 3000
+  }
+
+const NUC = {
+    name: "Windows based computer",
+    CPU: "Intel",
+    GPU: {
+        buildIn: "Intel",
+        external: "AMD"
+    },
+    numUSB: 4,
+    numHDMI: 1,
+    numUSBC: 1,
+    memory: {
+        firstSlot: mem,
+        secondSlot: mem
+    },
+    powerOn: false,
+    togglePower: function (powerStatus) {
+      this.powerOn = powerStatus;
+    },
+  };
