33c1b1e9c165bc532c286b3587970dea239759f5
jpunl <s-jpu2@unl.edu>
2023-03-03 12:15:26 +1100

    complete 05_10

--- 05_10/script.js
+++ 05_10/script.js
@@ -29,34 +29,47 @@ const content = `
     </figure>
     <h1 class="backpack__name">Everyday Backpack</h1>
     <ul class="backpack__features">
-      <li class="packprop backpack__volume">Volume:<span> ${
-        everydayPack.volume
-      }l</span></li>
-      <li class="packprop backpack__color">Color:<span> ${
-        everydayPack.color
-      }</span></li>
+      <li class="packprop backpack__volume">Volume:<span> ${everydayPack.volume
+  }l</span></li>
+      <li class="packprop backpack__color">Color:<span> ${everydayPack.color
+  }</span></li>
       <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
-      <li class="packprop backpack__pockets">Number of pockets:<span> ${
-        everydayPack.pocketNum
-      }</span></li>
-      <li class="packprop backpack__strap">Left strap length:<span> ${
-        everydayPack.strapLength.left
-      } inches</span></li>
-      <li class="packprop backpack__strap">Right strap length:<span> ${
-        everydayPack.strapLength.right
-      } inches</span></li>
-      <li class="packprop backpack__lid">Lid status:<span> ${
-        everydayPack.lidOpen
-      }</span></li>
+      <li class="packprop backpack__pockets">Number of pockets:<span> ${everydayPack.pocketNum
+  }</span></li>
+      <li class="packprop backpack__strap">Left strap length:<span> ${everydayPack.strapLength.left
+  } inches</span></li>
+      <li class="packprop backpack__strap">Right strap length:<span> ${everydayPack.strapLength.right
+  } inches</span></li>
+      <li class="packprop backpack__lid">Lid status:<span> ${everydayPack.lidOpen
+  }</span></li>
     </ul>
   
 `;
 
+
+const navContent = `
+      <li><a href="#">number 1</a></li>
+      <li><a href="#">number 2</a></li>
+      <li><a href="#">number 3</a></li>
+      <li><a href="#">number 4</a></li>
+      <li><a href="#">number 5</a></li>
+`;
+
 const main = document.querySelector(".maincontent");
+const head = document.querySelector(".siteheader");
 
 const newArticle = document.createElement("article");
 newArticle.classList.add("backpack");
 newArticle.setAttribute("id", "everyday");
 newArticle.innerHTML = content;
 
+const newNav = document.createElement("nav");
+newNav.classList.add("NewNavi")
+
+const navList = document.createElement("ul");
+navList.innerHTML = navContent;
+newNav.append(navList);
+
+
+head.append(newNav);
 main.append(newArticle);
