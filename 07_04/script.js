1210c941251c7f7286ceb2d4e0955eb9fd2fdb4d
jpunl <s-jpu2@unl.edu>
2023-03-03 16:08:06 +1100

    complete 07_04

--- 07_04/script.js
+++ 07_04/script.js
@@ -9,3 +9,28 @@
  */
 
 
+let desk = ["CD", "pen", "book", "eraser", "ball", "keyboard", "mouse", "speaker"];
+
+console.log(desk);
+let temp = desk.pop();
+console.log(desk);
+console.log(temp);
+desk.unshift(temp);
+console.log(desk);
+
+
+sortedDesk = [...desk].sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+console.log(sortedDesk);
+
+const itemToFind = "ball";
+const foundItem = desk.find(item => item === itemToFind);
+console.log(foundItem);
+
+const indexToRemove = desk.indexOf(foundItem);
+console.log(indexToRemove);
+
+if (indexToRemove !== -1) {
+    desk.splice(indexToRemove, 1);
+}
+
+console.log(desk);
