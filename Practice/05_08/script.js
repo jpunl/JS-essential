76e96c547db0cb0540fb9b8a7b42642ad1208896
jpunl <s-jpu2@unl.edu>
2023-03-03 11:21:13 +1100

    complete prac 05_08

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,5 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+const frogElement = document.querySelector("#pack02")
