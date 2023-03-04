20083f92e2da935b28568d95d969bbf612314d09
jpunl <s-jpu2@unl.edu>
2023-03-04 23:54:23 +1100

    modified 08_11

--- 08_11/script.js
+++ 08_11/script.js
@@ -24,25 +24,21 @@ const content = `
   </figure>
   <h1 class="backpack__name">${everydayPack.name}</h1>
   <ul class="backpack__features">
-    <li class="feature backpack__volume">Volume:<span> ${
-      everydayPack.volume
-    }l</span></li>
-    <li class="feature backpack__color">Color:<span> ${
-      everydayPack.color
-    }</span></li>
+    <li class="feature backpack__volume">Volume:<span> ${everydayPack.volume
+  }l</span></li>
+    <li class="feature backpack__color">Color:<span> ${everydayPack.color
+  }</span></li>
     <li class="feature backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
-    <li class="feature backpack__pockets">Number of pockets:<span> ${
-      everydayPack.pocketNum
-    }</span></li>
-    <li class="feature backpack__strap">Left strap length:<span> ${
-      everydayPack.strapLength.left
-    } inches</span></li>
-    <li class="feature backpack__strap">Right strap length:<span> ${
-      everydayPack.strapLength.right
-    } inches</span></li>
+    <li class="feature backpack__pockets">Number of pockets:<span> ${everydayPack.pocketNum
+  }</span></li>
+    <li class="feature backpack__strap">Left strap length:<span> ${everydayPack.strapLength.left
+  } inches</span></li>
+    <li class="feature backpack__strap">Right strap length:<span> ${everydayPack.strapLength.right
+  } inches</span></li>
     <li class="feature backpack__lid">Lid status:<span> ${
-      everydayPack.lidOpen
-    }</span></li>
+  // everydayPack.lidOpen
+  everydayPack.lidOpen ? "open" : "closed"
+  }</span></li>
   </ul>
 `;
 
@@ -54,3 +50,11 @@ newArticle.setAttribute("id", "everyday");
 newArticle.innerHTML = content;
 
 main.append(newArticle);
+
+if (everydayPack.lidOpen) {
+  console.log("Lid is open!");
+} else {
+  console.log("lid is closed");
+}
+
+console.log(everydayPack.lidOpen ? "open" : "closed");
